66bb26471ad43f8bdfc1e50ea11c7ce2cf9e7a02
Milton Higaki <milton.higaki@gmail.com>
2019-08-11 00:12:25 -0300

    form contato: reinaldo.plasma@gmail.com

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -32,7 +32,8 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "milton.higaki@gmail.com";
+//$EmailTo = "milton.higaki@gmail.com";
+$EmailTo = "reinaldo.plasma@gmail.com";
 $Subject = "Website - contato";
 
 // prepare email body text
